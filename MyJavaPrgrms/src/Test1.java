0691a46e1d020dd0778076f911bdd9cce53dc6c4
Giddaluru-Sireesha <giddalurusireesha@gmail.com>
2022-03-08 19:34:39 +0530

    Test1 file modified

--- MyJavaPrgrms/src/Test1.java
+++ MyJavaPrgrms/src/Test1.java
@@ -4,6 +4,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("hello world");
+   int x=100;
+   System.out.println("X valye is:"+x);
 	}
 
 }
